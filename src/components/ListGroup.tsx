af19df1889b8a51fbda3ea400e91f4aa4de9e441
Yaokun <yiukuenchu@gmail.com>
2023-11-23 16:53:51 +1030

    Implement ListGroup component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,44 @@
+import { Fragment, useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  // Conditional rendering
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
+
+  return (
+    <Fragment>
+      <h1>{heading}</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export default ListGroup;
